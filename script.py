bf456a05600f183aae927da79fb31ab87abf36a3
Rhyme <rhyme1325@gmail.com>
2022-12-24 01:29:01 +1100

    script

--- script.py
+++ script.py
@@ -1,16 +1,21 @@
 import os
 from datetime import date
+import random
 
 os.system('git pull')
 
-time = "\n" + date.today().strftime("%Y-%m-%d %H:%M:%S")
-# get current time
-f = open("dependencies/lineage.txt", "a")
-f.write(time)
-f.close()
+stack = [1, 7, 14, 28, 56]
+chosen = random.randint(0,4)
 
-os.system('git add .')
+for i in range(stack[chosen]):
+    time = "\n" + date.today().strftime("%Y-%m-%d %H:%M:%S")
+    # get current time
+    f = open("dependencies/lineage.txt", "a")
+    f.write(time)
+    f.close()
 
-os.system('git commit -m "script" ')
+    os.system('git add .')
 
-os.system('git push')
+    os.system('git commit -m "script" ')
+
+    os.system('git push')
