cc70a54295e0dc5018c678ca29bab7ee4c9fe6b9
Rhyme <rhyme1325@gmail.com>
2022-12-24 00:47:48 +1100

    v

--- naive.py
+++ naive.py
@@ -7,6 +7,8 @@ f = open("dependencies/lineage.txt", "a")
 f.write("Now the file has more content!")
 f.close()
 
+os.system('git add .')
+
 os.system('git commit -m "script" ')
 
 os.system('git push')
