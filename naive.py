a02ef1c9f846c0f0e3890a7c53f9a5b6739c856f
Rhyme <rhyme1325@gmail.com>
2022-12-24 00:49:54 +1100

    fix newline

--- naive.py
+++ naive.py
@@ -4,7 +4,7 @@ os.system('git pull')
 
 # get current time
 f = open("dependencies/lineage.txt", "a")
-f.write("Now the file has more content!\n")
+f.write("\nNow the file has more content!")
 f.close()
 
 os.system('git add .')
