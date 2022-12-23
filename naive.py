65887bf74fdb02a58be659a6d052e001474a6711
Rhyme <rhyme1325@gmail.com>
2022-12-24 00:58:04 +1100

    script

--- naive.py
+++ naive.py
@@ -1,17 +1,1 @@
-import os
-from datetime import date
 
-os.system('git pull')
-
-time = "\n" + date.today().strftime("%Y-%m-%d %H:%M:%S")
-# get current time
-f = open("dependencies/lineage.txt", "a")
-f.write(time)
-f.write("\nNow the file has more content!")
-f.close()
-
-os.system('git add .')
-
-os.system('git commit -m "script" ')
-
-os.system('git push')
--- script.py
+++ script.py
@@ -1,4 +1,17 @@
+import os
 from datetime import date
 
-today = date.today().strftime("%Y-%m-%d %H:%M:%S")
-print("Today's date:", today)
+os.system('git pull')
+
+time = "\n" + date.today().strftime("%Y-%m-%d %H:%M:%S")
+# get current time
+f = open("dependencies/lineage.txt", "a")
+f.write(time)
+f.write("\nNow the file has more content!")
+f.close()
+
+os.system('git add .')
+
+os.system('git commit -m "script" ')
+
+os.system('git push')
